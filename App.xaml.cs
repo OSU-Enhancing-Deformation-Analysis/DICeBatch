2d74c8c2f245862804a3c4d175a31c07169766e3
Brock Cloutier <brockcloutier@gmail.com>
2026-01-03 21:20:40 -0800

    inital push

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DICeBatch
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
